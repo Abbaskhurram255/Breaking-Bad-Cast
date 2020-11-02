3974635ea65cee7240ad030c338456d5dc45988c
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-03 02:47:01 +0500

    Updates

---
+++ precache-manifest.16e83e99e14a66e6badc946af9777d26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea35b78c2f647137e37ed06d2691db3",
+    "url": "/Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "7117a3869394dc6cb763",
+    "url": "/Breaking-Bad-Cast/static/css/main.f75b5148.chunk.css"
+  },
+  {
+    "revision": "fa757ae8f448440bad00",
+    "url": "/Breaking-Bad-Cast/static/js/2.7f57ec12.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Breaking-Bad-Cast/static/js/2.7f57ec12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7117a3869394dc6cb763",
+    "url": "/Breaking-Bad-Cast/static/js/main.9300222d.chunk.js"
+  },
+  {
+    "revision": "a3b6a786245ad3955008",
+    "url": "/Breaking-Bad-Cast/static/js/runtime-main.8015c9c4.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
